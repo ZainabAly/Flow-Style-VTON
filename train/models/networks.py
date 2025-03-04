6e4d09c2c0463cd2a8abdd27575c4e861475dac3
ZeinabAly <133286101+ZainabAly@users.noreply.github.com>
2025-03-04 15:17:47 +0200

    Update networks.py

--- train/models/networks.py
+++ train/models/networks.py
@@ -119,55 +119,40 @@ class ResUnetSkipConnectionBlock(nn.Module):
             return torch.cat([x, self.model(x)], 1)
 
 
-class Vgg19(nn.Module):
-    def __init__(self, requires_grad=False):
-        super(Vgg19, self).__init__()
-        vgg_pretrained_features = models.vgg19(pretrained=True).features
-        self.slice1 = nn.Sequential()
-        self.slice2 = nn.Sequential()
-        self.slice3 = nn.Sequential()
-        self.slice4 = nn.Sequential()
-        self.slice5 = nn.Sequential()
-        for x in range(2):
-            self.slice1.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(2, 7):
-            self.slice2.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(7, 12):
-            self.slice3.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(12, 21):
-            self.slice4.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(21, 30):
-            self.slice5.add_module(str(x), vgg_pretrained_features[x])
+import timm
+
+class TimmFeatureExtractor(nn.Module):
+    def __init__(self, model_name='swin_base_patch4_window7_224', requires_grad=False):
+        super(TimmFeatureExtractor, self).__init__()
+        self.model = timm.create_model(model_name, pretrained=True, features_only=True)
+        
         if not requires_grad:
             for param in self.parameters():
                 param.requires_grad = False
 
-    def forward(self, X):
-        h_relu1 = self.slice1(X)
-        h_relu2 = self.slice2(h_relu1)
-        h_relu3 = self.slice3(h_relu2)
-        h_relu4 = self.slice4(h_relu3)
-        h_relu5 = self.slice5(h_relu4)
-        out = [h_relu1, h_relu2, h_relu3, h_relu4, h_relu5]
-        return out
+    def forward(self, x):
+        features = self.model(x)
+        return features  # Returns a list of feature maps at different depths
 
-class VGGLoss(nn.Module):
-    def __init__(self, layids = None):
-        super(VGGLoss, self).__init__()
-        self.vgg = Vgg19()
-        self.vgg.cuda()
+
+class TimmLoss(nn.Module):
+    def __init__(self, model_name='swin_base_patch4_window7_224', layer_ids=None):
+        super(TimmLoss, self).__init__()
+        self.feature_extractor = TimmFeatureExtractor(model_name)
+        self.feature_extractor.cuda()
         self.criterion = nn.L1Loss()
-        self.weights = [1.0/32, 1.0/16, 1.0/8, 1.0/4, 1.0]
-        self.layids = layids
+        self.weights = [1.0 / (2**i) for i in range(5)]  # Adjust weights if necessary
+        self.layer_ids = layer_ids if layer_ids is not None else list(range(5))
 
     def forward(self, x, y):
-        x_vgg, y_vgg = self.vgg(x), self.vgg(y)
+        x_features = self.feature_extractor(x)
+        y_features = self.feature_extractor(y)
+        
         loss = 0
-        if self.layids is None:
-            self.layids = list(range(len(x_vgg)))
-        for i in self.layids:
-            loss += self.weights[i] * self.criterion(x_vgg[i], y_vgg[i].detach())
+        for i in self.layer_ids:
+            loss += self.weights[i] * self.criterion(x_features[i], y_features[i].detach())
         return loss
+
 
 def save_checkpoint(model, save_path):
     if not os.path.exists(os.path.dirname(save_path)):
